b098c25e55bc80af80f909daceea940273ee7830
Ben Asaf <ToBenAsaf@Gmail.com>
2021-11-02 11:03:09 +0900

    Added first draft of the Fold BN

--- dense_optimization/transforms/fold_bn.py
+++ dense_optimization/transforms/fold_bn.py
@@ -1,5 +1,19 @@
+import tensorflow as tf
+import numpy as np
 
 
+def fold_batch_norm_conv2d(conv: tf.keras.layers.Conv2D, bn: tf.keras.layers.BatchNormalization, epsilon=1e-6):
+    gamma = bn.gamma.numpy()
+    gamma = gamma.reshape((1, 1, 1, gamma.shape[0]))
+    beta = bn.beta.numpy()
+    mean = bn.moving_mean.numpy()
+    variance = bn.moving_variance.numpy()
+    variance = variance.reshape((1, 1, 1, variance.shape[0]))
 
-def add(x, y):
-    return x+y - 2
+    bottom_part = np.sqrt(variance + epsilon)
+
+    new_kernel = (conv.kernel * gamma) / bottom_part
+    conv.kernel.assign(new_kernel)
+
+    new_bias = beta + (conv.bias - mean) * gamma / bottom_part
+    conv.bias.assign(tf.reshape(new_bias, (-1, )))
--- tests/transforms/test_fold_bn.py
+++ tests/transforms/test_fold_bn.py
@@ -1,10 +1,30 @@
 import pytest
-from dense_optimization.transforms.fold_bn import add
+import os
+os.environ["CUDA_VISIBLE_DEVICeS"] = ""
+
+import tensorflow as tf
+import numpy as np
+from dense_optimization.transforms.fold_bn import fold_batch_norm_conv2d
+
 
 class TestFoldBN(object):
 
     def test_fold_bn(self):
-        x = 5
-        y = 3
-        result = add(x, y)
-        assert (x+y-3) == result
+        input_shape = [1, 80, 80, 1]
+        dummy_input = tf.random.uniform(input_shape, dtype=tf.float32)
+        conv_layer = tf.keras.layers.Conv2D(filters=3, kernel_size=(3, 3), )
+        bn_layer = tf.keras.layers.BatchNormalization()
+        _ = bn_layer(conv_layer(dummy_input))
+
+        conv_layer.bias.assign(tf.random.uniform(conv_layer.bias.shape))
+        conv_layer.kernel.assign(tf.random.uniform(conv_layer.kernel.shape))
+        bn_layer.gamma.assign(tf.random.uniform(bn_layer.gamma.shape))
+        bn_layer.beta.assign(tf.random.uniform(bn_layer.beta.shape))
+        bn_layer.moving_mean.assign(tf.random.uniform(bn_layer.moving_mean.shape))
+        bn_layer.moving_variance.assign(tf.random.uniform(bn_layer.moving_variance.shape))
+
+        result_before = bn_layer(conv_layer(dummy_input), training=False)
+        fold_batch_norm_conv2d(conv_layer, bn_layer)
+        results_after = conv_layer(dummy_input)
+
+        assert np.allclose(result_before.numpy(), results_after.numpy(), atol=1e-1)
